38718cf61e1a9ab991932607721af9ed5f57d3a3
reza prawira <prawira.reza@gmail.com>
2023-02-14 12:10:48 +0700

    release v0.0.2 update footer

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -14,7 +14,7 @@ export class Settings {
 export class AppSettings {
     public settings = new Settings(
         'Emporium',  // theme name
-        'green',     // green, blue, red, pink, purple, grey
+        'blue',     // green, blue, red, pink, purple, grey
         false,       // true = rtl, false = ltr
         true,        // adminSidenavIsOpened
         true,        // adminSidenavIsPinned 
