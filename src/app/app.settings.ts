1fbca9cde750cbf5df9d913b9c895ab93442875c
rezapr <prawira.reza@gmail.com>
2024-02-13 12:27:04 +0700

    init update

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -13,8 +13,8 @@ export class Settings {
 @Injectable()
 export class AppSettings {
     public settings = new Settings(
-        'Emporium',  // theme name
-        'green',     // green, blue, red, pink, purple, grey
+        'Amazing',  // theme name
+        'grey',     // green, blue, red, pink, purple, grey
         false,       // true = rtl, false = ltr
         true,        // adminSidenavIsOpened
         true,        // adminSidenavIsPinned 
